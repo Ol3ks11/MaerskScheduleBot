274f2f5ec10dc0cfab33be7b977cac4151f93c95
Ol3ks11 <114064036+Ol3ks11@users.noreply.github.com>
2022-10-04 01:18:23 +0300

    Update Program.cs

--- MaerskScheduleBot/Program.cs
+++ MaerskScheduleBot/Program.cs
@@ -8,7 +8,7 @@ internal class Program
     {
         string activeVesselsJsonPath = Path.Combine(Environment.CurrentDirectory, @"Resources\active.json");
         VesselsManager vesselsManager = JsonConvert.DeserializeObject<VesselsManager>(System.IO.File.ReadAllText(activeVesselsJsonPath));
-        TelegramBotClient telegramClient = new("5527446858:AAFnOJ4Y0w3a9ZB-cu4WtVMZEssBNyRLEPU");
+        TelegramBotClient telegramClient = new("Token");
         TelegramHandler telegramHandler = new(telegramClient, vesselsManager);
         telegramHandler.client.StartReceiving(telegramHandler.TelegramUpdate, telegramHandler.TelegramError);
 
